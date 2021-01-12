2b03bea1b22a72a464ef16dae74ac946eb3943c9
aamit2267 <aamit2267@gmail.com>
2021-01-12 17:24:56 +0530

    Updates

---
+++ precache-manifest.f02c6ab7477231db3a5a64cc9ad66074.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = [
+  {
+    "revision": "54b75efd37f00d8bbf15",
+    "url": "/my-profile/static/css/main.f7f36a4f.chunk.css"
+  },
+  {
+    "revision": "54b75efd37f00d8bbf15",
+    "url": "/my-profile/static/js/main.54b75efd.chunk.js"
+  },
+  {
+    "revision": "fae732bcea3a96128b63",
+    "url": "/my-profile/static/js/1.fae732bc.chunk.js"
+  },
+  {
+    "revision": "642da487a4a1cf3c825a",
+    "url": "/my-profile/static/js/runtime~main.642da487.js"
+  },
+  {
+    "revision": "bd882a417193d5b7ba3e4faaca2deff2",
+    "url": "/my-profile/static/media/logo.bd882a41.svg"
+  },
+  {
+    "revision": "9cd04089414e33881ad51eaaf10d2522",
+    "url": "/my-profile/static/media/cloud.9cd04089.svg"
+  },
+  {
+    "revision": "c6dde47a2f87dc8aeeaf2185c3a675f8",
+    "url": "/my-profile/static/media/portfolio.c6dde47a.png"
+  },
+  {
+    "revision": "b1c58ee19d1e2c38b29ae86f3174f2e4",
+    "url": "/my-profile/static/media/instagram.b1c58ee1.svg"
+  },
+  {
+    "revision": "deda33aea2bfa9216df4b9abc2d17698",
+    "url": "/my-profile/static/media/gmail.deda33ae.svg"
+  },
+  {
+    "revision": "6f0f54fb3b6daf2ca7f31fe5858971e4",
+    "url": "/my-profile/static/media/layer_1.6f0f54fb.svg"
+  },
+  {
+    "revision": "471bf2239fa46a60415eb89170d724a3",
+    "url": "/my-profile/static/media/layer_2.471bf223.svg"
+  },
+  {
+    "revision": "e0f44f2da02385136d645af555d1f6a1",
+    "url": "/my-profile/static/media/layer_3.e0f44f2d.svg"
+  },
+  {
+    "revision": "d76f88fac97a987fa73a1baba482d0ac",
+    "url": "/my-profile/static/media/layer_4.d76f88fa.svg"
+  },
+  {
+    "revision": "10fd1f92190fa43b091fd89bafc10498",
+    "url": "/my-profile/static/media/pique.10fd1f92.svg"
+  },
+  {
+    "revision": "53ead24efdd80af48877754697d4499d",
+    "url": "/my-profile/static/media/uproar.53ead24e.svg"
+  },
+  {
+    "revision": "77577941f9d32c2cf5c2acb6bd95aa9f",
+    "url": "/my-profile/static/media/spelling.77577941.svg"
+  },
+  {
+    "revision": "51b778663997bd247a70bef7bcb8b336",
+    "url": "/my-profile/static/media/snippets.51b77866.svg"
+  },
+  {
+    "revision": "f17d110d5c2701d47e927175493fec13",
+    "url": "/my-profile/static/media/todo.f17d110d.svg"
+  },
+  {
+    "revision": "8f178b628a65b655394de31291319410",
+    "url": "/my-profile/static/media/typing.8f178b62.svg"
+  },
+  {
+    "revision": "4eaf4ef3689634d98fe761136d883c55",
+    "url": "/my-profile/static/media/resume-img.4eaf4ef3.pdf"
+  },
+  {
+    "revision": "b5926c87cd093fb2dc33779b9672c527",
+    "url": "/my-profile/static/media/resume-img.b5926c87.png"
+  },
+  {
+    "revision": "cdf1bf0a08ce19568ce21597332f7e8c",
+    "url": "/my-profile/static/media/android.cdf1bf0a.svg"
+  },
+  {
+    "revision": "4d785e947345a30360b0571979e70fdf",
+    "url": "/my-profile/static/media/css.4d785e94.svg"
+  },
+  {
+    "revision": "23d81d6122e67c3d1eb787bd0ae2d178",
+    "url": "/my-profile/static/media/express.23d81d61.svg"
+  },
+  {
+    "revision": "0804e31ed83b8506db423f0841545090",
+    "url": "/my-profile/static/media/figma.0804e31e.svg"
+  },
+  {
+    "revision": "d82014167a81640b1ec8ea16ad2a196f",
+    "url": "/my-profile/static/media/flutter.d8201416.svg"
+  },
+  {
+    "revision": "838cf58c0be039825232e8e724b5bab1",
+    "url": "/my-profile/static/media/git.838cf58c.svg"
+  },
+  {
+    "revision": "0caacd80fa627ad5c76ff9525f6236d4",
+    "url": "/my-profile/static/media/html.0caacd80.svg"
+  },
+  {
+    "revision": "b887e502c6e09de74da5a53bf7723aab",
+    "url": "/my-profile/static/media/illustrator.b887e502.svg"
+  },
+  {
+    "revision": "42e31620aa32f13cf7849618fc4133b0",
+    "url": "/my-profile/static/media/indesign.42e31620.svg"
+  },
+  {
+    "revision": "4b48442d2f2cc7d651c013a33360f0d8",
+    "url": "/my-profile/static/media/js.4b48442d.svg"
+  },
+  {
+    "revision": "52783a2f8043e237279093badadc9cf1",
+    "url": "/my-profile/static/media/mongod.52783a2f.svg"
+  },
+  {
+    "revision": "4643494b26276d291b810d5610789aa7",
+    "url": "/my-profile/static/media/mongodb.4643494b.svg"
+  },
+  {
+    "revision": "f96ac8e52f6faa9b5898f091d097bf50",
+    "url": "/my-profile/static/media/node-js.f96ac8e5.svg"
+  },
+  {
+    "revision": "b0154b359ed2e29cb324cd2e163d0ab9",
+    "url": "/my-profile/static/media/photoshop.b0154b35.svg"
+  },
+  {
+    "revision": "a03a8833e53dfa6c581691d24c25557f",
+    "url": "/my-profile/static/media/react-js.a03a8833.svg"
+  },
+  {
+    "revision": "90d7190f522c05aed09d2d0486ea51df",
+    "url": "/my-profile/static/media/redux.90d7190f.svg"
+  },
+  {
+    "revision": "0b6ac59fc7757c1cdc58840074e71ea5",
+    "url": "/my-profile/static/media/sass.0b6ac59f.svg"
+  },
+  {
+    "revision": "17a3905fb223cdb37b22c98327bb7dce",
+    "url": "/my-profile/static/media/vscode.17a3905f.svg"
+  },
+  {
+    "revision": "982e0ecbbcfad621ec611a0a8acf0adb",
+    "url": "/my-profile/static/media/webpack.982e0ecb.svg"
+  },
+  {
+    "revision": "ac9c269a27f4fafc3f8bc57f6c10ba69",
+    "url": "/my-profile/static/media/bg.ac9c269a.svg"
+  },
+  {
+    "revision": "103738b7610a943dbe041dbda27f2be2",
+    "url": "/my-profile/index.html"
+  }
+];
